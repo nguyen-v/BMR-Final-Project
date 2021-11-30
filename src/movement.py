ae9bbbc5a834e0ea8d7e3cea168d7d8d546e9ead
Yassine Bakkali <ybakkali8@gmail.com>
2021-11-30 15:29:43 +0100

    implemented base state machine

--- src/movement.py
+++ src/movement.py
@@ -33,7 +33,7 @@ MAP_WIDTH = 640
 
 MAP_LENGTH = 480
 
-ROTATION_THRESHOLD = 30 # degrees
+ROTATION_THRESHOLD = 15 # degrees
 
 # threading.Timer(0.0333,camera_feed)
 
@@ -77,31 +77,61 @@ def initialization():
     # Calculate Global Path (Function required)
     return binary_map, rect_map, map, map_enlarged
 
-def follow_path():
+def follow_path(nodes, thymio_init_pos):
+
     last_angle = 0
     turn_direction = 1
     kalman = KF()
-    if camera_available:
-        angle = calculate_orientation(thymio_x, thymio_y, obj_x, obj_y) #should be returned by the local function
-        last_angle = angle
-    else:
-        angle = last_angle
-    a_thymio = calculate_obj_angle(thymio_pos)
-    a_obj = calculate_obj_angle(obj_pos)
-    if a_thymio - a_obj > 0:
-        if a_thymio - a_obj <= math.pi/2:
-            turn_direction = 1
-        if a_thymio - a_obj > math.pi/2:
-            turn_direction = -1
-    elif a_thymio - a_obj < 0:
-        if a_thymio - a_obj <= -math.pi/2:
-            turn_direction = 1
-        if a_thymio - a_obj > -math.pi/2:
-            turn_direction = -1
-    sens.thymio_rotate(angle, turn_direction)
-    KF.save_input_control(-vx, -vy) #save input controls, stop the motor
-    X_next = KF.kalman_filter()
+    last_pos = thymio_init_pos
 
+    for i in range(len(nodes)):
+        dist = calculate_distance(thymio_init_pos,nodes[i])
+        est_dist = 0
+
+        if camera_available(): # Please someone implement this function
+            angle = calculate_orientation(thymio_x, thymio_y, obj_x, obj_y) #should be returned by the local function
+            last_angle = angle
+        else:
+            angle = last_angle
+
+        a_thymio = calculate_obj_angle(thymio_pos)
+        a_obj = calculate_obj_angle(obj_pos)
+
+        if a_thymio - a_obj > 0:
+            if a_thymio - a_obj <= math.pi/2:
+                turn_direction = 1
+            if a_thymio - a_obj > math.pi/2:
+                turn_direction = -1
+        elif a_thymio - a_obj < 0:
+            if a_thymio - a_obj <= -math.pi/2:
+                turn_direction = 1
+            if a_thymio - a_obj > -math.pi/2:
+                turn_direction = -1
+
+        sens.thymio_rotate(angle, turn_direction)
+        #KF.save_input_control(-vx, -vy) #save input controls, stop the motor
+
+        while dist >= est_dist:
+            X_next = KF.kalman_filter()
+            est_dist = est_dist + calculate_distance(last_pos,X_next)
+            last_pos = X_next
+
+def thymio_move():
+    state == "localization"
+    binary_map, rect_map, map, map_enlarged = initialization()
+    while True:
+        if state == "localization":
+            nodes = global_path(map_enlarged, state)
+        elif state == "follow_path":
+            follow_path(nodes, state)
+        elif state == "local_avoidance":
+            local_avoidance(state)
+        elif state == "End":
+            break
+
+def local_avoidance():
+    something = 1
+    return something
 
 # Result angle is between pi and -pi
 def calculate_orientation(thymio_x, thymio_y, obj_x, obj_y):
