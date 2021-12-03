77d6d5d1a7b9cfd4a6f4d71d8f08ec988b8fc665
Vincent <vincen7.nguyen@gmail.com>
2021-12-03 19:17:35 +0100

    Local navigation working, changed hsv thresholds.

--- src/img_utils.py
+++ src/img_utils.py
@@ -12,6 +12,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # ========================================================================== #
+#  Global constants.                                                         # 
+# ========================================================================== #
+
+HSV_MAX_HUE = 180
+
+# ========================================================================== #
 #  Exported functions.                                                       # 
 # ========================================================================== #
 
@@ -21,21 +27,32 @@ import matplotlib.pyplot as plt
 #  @param HSV_THR_LOW   Low threshold in HSV space.
 #  @param HSV_THR_HIGH  High threshold in HSV space.
 #  @param exp_num_pts   Expected number of points (optional)
+#  @param is_red        Set to true if color to filter is red. This is a special case as red's hue lie between
+#                       0-10 and 170-180 (in OpenCV)
 #  @return dots         A list of (x, y) positions of dots. The size of the list is the same
 #                       as the number of expected points if the number of found points is greater than
 #                       the number of expected points. Points are sorted by size from biggest to smallest.
 #  @return found_pts    True if the number of found points is greater than the expected number of points.
-def get_color_dots(img, HSV_THR_LOW, HSV_THR_HIGH, exp_num_pts = None):
+def get_color_dots(img, HSV_THR_LOW, HSV_THR_HIGH, exp_num_pts = None, is_red = False):
     # convert to hsv space
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     # extract a certain color
     mask = cv2.inRange(hsv, HSV_THR_LOW, HSV_THR_HIGH)
+    mask_h = []
+    if is_red:
+        mask_h = cv2.inRange(hsv, (HSV_MAX_HUE - HSV_THR_HIGH[0], HSV_THR_LOW[1], HSV_THR_LOW[2]), (HSV_MAX_HUE, HSV_THR_HIGH[1], HSV_THR_HIGH[2]))
     # erode mask to avoid noise
+    if is_red:
+        mask = mask + mask_h
+    cv2.imshow('mask', mask)
+    cv2.waitKey(1)
     kernel = np.ones((2,2),np.uint8)
-    mask = cv2.erode(mask,kernel,iterations = 5)
+    mask = cv2.erode(mask,kernel,iterations = 3)
     # plt.figure()
     # plt.imshow(mask)
     # plt.show()
+    # cv2.imshow('mask', mask)
+    # cv2.waitKey(1)
     # extract contours of dots
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = list(contours)
--- src/test_vincent.py
+++ src/test_vincent.py
@@ -1,13 +1,3 @@
-# from thymio_connection import connect_to_thymio
-# from MyThymio import *
-# import time
-
-# thymio = MyThymio(verbose = True)
-
-# thymio.set_motor_left_speed(50)
-# time.sleep(1)
-# thymio.stop_thymio()
-
 import cv2
 import time
 from MyThymio import *
@@ -22,41 +12,60 @@ DELTA_T = 0.1
 # ========================================================================== #
 #  Main function.                                                            # 
 # ========================================================================== #
+def local_avoidance2(obj_pos, cam, M, rect_width, rect_height):
+    while True:
+        img, img_taken= take_picture(cam)
+        print(img_taken)
+        if img_taken:
+            img_rect = get_rectified_img(img, M, rect_width,  rect_height)
+            cv2.imshow('Window', img_rect)
+            cv2.waitKey(1)
+            thymio_pose, found_thymio = locate_thymio_camera(img_rect, "cartesian", (11, 7))
+            print("found thymio: {}".format(found_thymio))
+            print(thymio_pose)
+            if found_thymio:
+                y = np.zeros(2)
+                
+                # Local objective contribution (attractive)
+                a_th_obj = angle_two_points(thymio_pose[0], thymio_pose[1], obj_pos[0], obj_pos[1])
+                da = thymio_pose[2] - a_th_obj
+                if (da < -math.pi):
+                    da = da + 2*math.pi
+                print("delta angle {}".format(da))
+                y[0] = y[0] + OBJ_ATT_BASE + da/math.pi*OBJ_ATT_COEFF
+                y[1] = y[1] + OBJ_ATT_BASE - da/math.pi*OBJ_ATT_COEFF
 
+                # # Map obstacles contribution (repulsive)
+                # gnd_sens = np.divide(thymio.get_gnd_sensors(), GND_SCALE)
+                # we have to put a threshold on ground sensors if global obstacles have no gradient
+                # for i in range(NUM_GND_SENS):
+                #     y[0] = y[0] + gnd_sens[i] * WEIGHTS_LEFT_GND[i]
+                #     y[1] = y[1] + gnd_sens[i] * WEIGHTS_RIGHT_GND[i]
+                if math.dist([thymio_pose[0], thymio_pose[1]], [obj_pos[0], obj_pos[1]]) < LOC_DIST_THR:
+                    break
+            time.sleep(0.1)
+        time.sleep(0.1)
 
 def main():
 
     # Initialisations
     cam = init_camera()
-    # thymio = MyThymio()
     success = False
+    M = []
+    rect_height = 0
+    rect_width = 0
     while (success == False):
         img, img_taken = take_picture(cam)
+        
         if img_taken:
             M, rect_width, rect_height, map, map_enlarged, success = create_map(img, 11, 7)
-            # cv2.imshow('Window', img)
-            # cv2.waitKey(1)
         else:
             print("img not taken")
-        # print(img.shape)
-        # M, rect_width, rect_height, map, map_enlarged, success = create_map(img, 7, 11)
         time.sleep(DELTA_T)
-    plt.figure()
-    plt.imshow(map, origin = 'lower')
-    plt.title("Original Map")
-    plt.gca().invert_yaxis()
-    plt.show()
-    # # local_avoidance(thymio, obj_pos, cam, M, rect_width, rect_height)
-    while True:
-        img, img_taken= take_picture(cam)
-        if img_taken:
-            img_rect = get_rectified_img(img, M, rect_width,  rect_height)
-            thymio_pose, thymio_found = locate_thymio_camera(img_rect, "cartesian", (11, 7))
-            # print(thymio_found)
-            if thymio_found:
-                print("Thymio found: x: {} \t y: {} \t theta: {}".format(thymio_pose[0], thymio_pose[1], thymio_pose[2]))
-            cv2.imshow('Window', img_rect)
-            cv2.waitKey(1)
+        print("map created")
+    obj_pos = [100, 100]
+    thymio = MyThymio(verbose = True)
+    local_avoidance(thymio, obj_pos, cam, M, rect_width, rect_height)
 
 if __name__=="__main__":
     main()
